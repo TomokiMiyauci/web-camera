817f5c3d017ddfa8f87cb8aad0219d6a7ad9263a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-03 01:45:55 +0900

    :tada: Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { LicenseWebpackPlugin } =  require('license-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+/**@type{import('webpack').Configuration}*/
+const config = {
+    entry: path.resolve(__dirname, './src/index'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[hash:8].js',
+        chunkFilename: '[name].[chunkhash:8].js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+        }),
+        new LicenseWebpackPlugin({
+            addBanner: true
+        }),
+    ]
+}
+
+module.exports = config
