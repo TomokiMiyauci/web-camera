a270d10ad156793b969a82baf51ebd8f8700ab23
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-04 18:58:25 +0900

    :sparkles: Add camera page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
-const App = () => <h1>Hefffllo</h1>;
+import CameraPage from '~/components/camera/CameraPage';
+
+/**
+ * @typedef state
+ * @property {'camera'} page
+ */
+
+/** @extends {React.Component<(), State>} */
+class App extends React.Component {
+  /** @type {State} */
+  state = {
+    page: 'camera',
+  };
+
+  render() {
+    const { page } = this.state;
+    switch (page) {
+      case 'camera': {
+        return <CameraPage />;
+      }
+    }
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactDom from 'react-dom'
-import App from '~/App'
-import '~/global.css'
+import '~/development';
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from '~/App';
+import '~/global.css';
 
-ReactDom.render(
-  <App />,
-  document.getElementById('app')
-)
+ReactDom.render(<App />, document.getElementById('app'));
