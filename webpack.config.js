517809663d5e2240a5f345cf7cf76e1c87d23a90
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-04 17:19:23 +0900

    :wrench: Apply tab space is 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,58 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { LicenseWebpackPlugin } =  require('license-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { LicenseWebpackPlugin } = require('license-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 /**@type{import('webpack').Configuration}*/
 const config = {
-    entry: path.resolve(__dirname, './src/index'),
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].[hash:8].js',
-        chunkFilename: '[name].[chunkhash:8].js',
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
+  entry: path.resolve(__dirname, './src/index'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[hash:8].js',
+    chunkFilename: '[name].[chunkhash:8].js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
 
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-        }),
-        new LicenseWebpackPlugin({
-            addBanner: true
-        }),
-    ],
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+    new LicenseWebpackPlugin({
+      addBanner: true,
+    }),
+  ],
 
-    mode: process.env.mode || 'development',
+  mode: process.env.mode || 'development',
 
-    module: {
-        rules: [
-            {
-                test: /\.js/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
             },
-            {
-                test: /\.css/,
-                use: [
-                    'style-loader', 
-                    {   
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                            modules: true,
-                        }
-                    },
-                    'postcss-loader'
-                ]
-            }
-        ]
+          },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
     },
+  },
+};
 
-    resolve: {
-        alias: {
-            '~': path.resolve(__dirname, 'src')
-        }
-    }
-}
-
-module.exports = config
+module.exports = config;
